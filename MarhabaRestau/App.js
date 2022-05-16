6fb7f8ef8154bf3cf89167fd68b3368f9464982f
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-05-16 09:45:17 +0100

    import package in app.js

--- MarhabaRestau/App.js
+++ MarhabaRestau/App.js
@@ -1,4 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
   return (
